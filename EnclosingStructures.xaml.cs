883b3586274879ef80b184f7c0c8786834d67766
Iru <Iru@DESKTOP-HCAFA0E>
2023-08-27 12:54:37 +0300

    Добавьте файлы проекта.

---
+++ EnclosingStructures.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfEnergyCalculator
+{
+
+    public class StructureTableView
+    {
+        public string StructureName { get; set; }
+        public string ShortStructureName { get; set; }
+        public string StructureOrientation { get; set; }
+        public string StructureArea { get; set; }
+    }
+
+
+    /// <summary>
+    /// Логика взаимодействия для EnclosingStructures.xaml
+    /// </summary>
+    public partial class EnclosingStructures : Window
+    {
+
+        public ObservableCollection<StructureTableView> MyData { get; set; }
+        public ICommand ClickMyButton1 { get; set; }
+
+        public EnclosingStructures()
+        {
+            InitializeComponent();
+
+            // for binding
+            this.DataContext = this;
+
+            // data for grid
+            MyData = new ObservableCollection<StructureTableView>()
+               {
+                         new StructureTableView() {StructureName="Стена", ShortStructureName="НС1", StructureOrientation="С", StructureArea="100"},
+                         new StructureTableView() {StructureName="Окно", ShortStructureName="ОК1", StructureOrientation="С", StructureArea="10"}
+
+               };
+
+        }
+        private void ChangeText(object sender, RoutedEventArgs e)
+        {
+            StructureTableView item = EnclosingStructureForm.SelectedItem as StructureTableView;
+            if (item != null)
+            {
+                ThermalProperties thermal_property_view = new ThermalProperties(item.ShortStructureName);
+                thermal_property_view.Show();
+            }
+            else
+            {
+                MessageBox.Show("Не заполнены данные");
+            }
+        }
+
+    }
+
+}
---
+++ ThermalProperties.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfEnergyCalculator
+{
+    /// <summary>
+    /// Логика взаимодействия для ThermalProperties.xaml
+    /// </summary>
+    public partial class ThermalProperties : Window
+    {
+        public string Data { get; set; }
+        public ThermalProperties(string data)
+        {
+            Data = data;
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
